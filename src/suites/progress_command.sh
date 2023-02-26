6437ef4833ae6f371f8f190e7a848d433e2f1901
Daniel Brady <daniel.13rady@gmail.com>
2023-02-25 17:53:34 -1000

    update CLI build

--- src/suites/progress_command.sh
+++ src/suites/progress_command.sh
@@ -1,4 +1,4 @@
-echo "# this file is located in 'src/suites_progress_command.sh'"
+echo "# this file is located in 'src/suites/progress_command.sh'"
 echo "# code for 'cli suites progress' goes here"
 echo "# you can edit it freely and regenerate (it will not be overwritten)"
 inspect_args
